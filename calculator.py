066ab575c1378323c7ccb48285c4ea24d6837a4d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 15:56:00 -0700

    called all functions for operations

--- calculator.py
+++ calculator.py
@@ -16,18 +16,43 @@ from arithmetic import *
 
 def calculate():
     """takes in numbers and commands and calculates answer"""
+    
     while True:
         user_command = input(">")
+        # tokenizing user input
         command_list = user_command.split(" ")
-        operator = command_list[0]
-        num1 = int(comman_list[1])
+        operator = command_list[0].lower()
+        num1 = float(command_list[1])      
+        # accounting for inputs of different lengths
         if len(command_list) >= 3:
-            num2 = int(command_list[2])
+            num2 = float(command_list[2])
         if len(command_list) == 4:
-            num3 = int(command_list[3])
+            # num3 = float(command_list[3])
+            print("We are going to use your first 2 lovely numbers.")
 
+        # allowing to quit out of calculator
         if user_command == "q" or user_command == "quit":
             print("Good bye.")
             break
+        # calling functions in aritmetic.py
+        if operator == "+":
+            print(add(num1, num2))
+        elif operator == "-":
+            print(subtract(num1, num2))
+        elif operator == "*":
+            print(multiply(num1, num2))
+        elif operator == "/":
+            print(divide(num1, num2))
+        elif operator == "square":
+            print(square(num1))
+        elif operator == "cube":
+            print(cube(num1))
+        elif operator == "pow":
+            print(power(num1, num2))
+        elif operator == "mod":
+            print(mod(num1, num2))
+        else:
+            print("um. that's not a valid operator.")
+            print("try again. K thanks.")
 
-        if operator == ""
+calculate()
