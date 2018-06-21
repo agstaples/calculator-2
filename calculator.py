aafa21fab2cb030ec72ac2d2fca6b83876703f36
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 17:18:25 -0700

    Added reduce for infinite inputs and fixed decimal in output to 3

--- calculator.py
+++ calculator.py
@@ -12,47 +12,38 @@ calculator program yourself in this file.
 
 
 from arithmetic import *
+import functools
 
 
-def calculate():
-    """takes in numbers and commands and calculates answer"""
-    
-    while True:
-        user_command = input(">")
-        # allowing to quit out of calculator
-        if user_command == "q" or user_command == "quit":
-            print("Good bye.")
-            break
-        # tokenizing user input
-        command_list = user_command.split(" ")
-        operator = command_list[0].lower()
-        num1 = float(command_list[1])      
-        # accounting for inputs of different lengths
-        if len(command_list) >= 3:
-            num2 = float(command_list[2])
-        if len(command_list) == 4:
-            # num3 = float(command_list[3])
-            print("We are going to use your first 2 lovely numbers.")
+while True:
+    user_command = input(">")
+    # allowing to quit out of calculator
+    if user_command == "q" or user_command == "quit":
+        print("Good bye.")
+        break
+    # tokenizing user input
+    commands = user_command.split()
+    numbers = [float(i) for i in commands[1:]]
+    operator = commands[0].lower()
+    num1 = float(commands[1])
 
-        # calling functions in aritmetic.py
-        if operator == "+":
-            print(add(num1, num2))
-        elif operator == "-":
-            print(subtract(num1, num2))
-        elif operator == "*":
-            print(multiply(num1, num2))
-        elif operator == "/":
-            print(divide(num1, num2))
-        elif operator == "square":
-            print(square(num1))
-        elif operator == "cube":
-            print(cube(num1))
-        elif operator == "pow":
-            print(power(num1, num2))
-        elif operator == "mod":
-            print(mod(num1, num2))
-        else:
-            print("um. that's not a valid operator.")
-            print("try again. K thanks.")
-
-calculate()
+    # calling functions in aritmetic.py
+    if operator == "+":
+        print(round(functools.reduce(add, numbers), 3))
+    elif operator == "-":
+        print(round(functools.reduce(subtract, numbers), 3))
+    elif operator == "*":
+        print(round(functools.reduce(multiply, numbers), 3))
+    elif operator == "/":
+        print(round(functools.reduce(divide, numbers), 3))
+    elif operator == "square":
+        print(round(square(num1), 3))
+    elif operator == "cube":
+        print(round(cube(num1), 3))
+    elif operator == "pow":
+        print(round(functools.reduce(power, numbers), 3))
+    elif operator == "mod":
+        print(round(functools.reduce(mod, numbers), 3))
+    else:
+        print("um. that's not a valid operator.")
+        print("try again. K thanks.")
