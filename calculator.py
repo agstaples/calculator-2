6aed37234c31ea7a699b93307ae7f56053bfda1d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 17:59:39 -0700

    stylized dictionary

--- calculator.py
+++ calculator.py
@@ -26,7 +26,14 @@ while True:
     numbers = [float(i) for i in commands[1:]]
     operator = commands[0].lower()
     num1 = float(commands[1])
-    calc_dict = {"+": add, "-": subtract, "*": multiply, "/": divide, "pow": power, "mod": mod}
+    calc_dict = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide,
+        "pow": power,
+        "mod": mod,
+    }
 
     # calling functions in aritmetic.py
     if operator in calc_dict:
@@ -38,17 +45,3 @@ while True:
     else:
         print("um. that's not a valid operator.")
         print("try again. K thanks.")
-
-
-"""    if operator == "+":
-        print(round(functools.reduce(add, numbers), 3))
-    elif operator == "-":
-        print(round(functools.reduce(subtract, numbers), 3))
-    elif operator == "*":
-        print(round(functools.reduce(multiply, numbers), 3))
-    elif operator == "/":
-        print(round(functools.reduce(divide, numbers), 3))"""
-"""    elif operator == "pow":
-        print(round(functools.reduce(power, numbers), 3))
-    elif operator == "mod":
-        print(round(functools.reduce(mod, numbers), 3))"""
